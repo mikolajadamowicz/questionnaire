f088bd5342becaad262301a7fd9b9c5a79ba1996
mikolaj adamowicz <mr.ming122@gmail.com>
2018-05-07 11:07:11 +0200

    cos tam zmienone

--- routes/routes.js
+++ routes/routes.js
@@ -5,39 +5,43 @@ const appRouter = (app, db) => {
     app.get("/", (req, res) => {
         res.status(200).send("Welcome to our restful API, Aloha")
     })
-    // app.get("/getSurveys/:id", function(req, res) {
-    //   const id = req.params.id
 
-    //   const details = { _id: new ObjectID(id) }
-    //   db.collection("surveys").findOne(details, (err, item) => {
-    //     if (err) {
-    //       res.send({ error: "An error has occurred" })
-    //     } else {
-    //       res.send(item)
-    //     }
-    //   })
-    // })
     app.get("/getSurveys", (req, res) => {
-    //   db.collection("surveys").findOne({}, function(err, result) {
-    //     if (err) throw err
-    //     console.log(result)
-    //     res.send(result)
-    //   })
-    var collection = db.collection("surveys");
-    var cursor = collection
+      //TODO: zrobic tak zeby zwracal JSONA 
+    const collection = db.collection("surveys")
+    const cursor = collection
       .find()
       .limit(1)
-      .sort({ $natural: -1 });
+      .sort({ $natural: -1 })
 
-    cursor.toArray(function(err, results) {
-      if (err) throw err;
-      console.log("%j", results);
-    });
+    cursor.toArray((err, results) => {
+      if (err) throw err
+      console.log("%j", results)
+      res.setHeader("Content-Type", "application/json")
+      res.status(200).send(results)
     })
-    app.post("/postAnswer", (req, res) => {
-          console.log(req.body) // your JSON
-          res.send(req.body)     
     })
+
+    app.put("/answer/:id", (req, res) => {
+      const id = req.params.id
+      const details = { _id: new ObjectID(id) }
+      const answer = { 
+        text: req.body.text, 
+        title: req.body.title,
+        forCount: req.body.forCount,
+        againstCount: req.body.againstCount
+      }
+      console.log(req.body)
+      db.collection("surveys").update(details, answer, (err, result) => {
+        if (err) {
+          res.send({ error: "An error has occurred" })
+        } else {
+          // console.log(req.body)
+          res.status(200).send(answer)
+        }
+      })
+    })    
+
     app.post("/addSurvey", (req, res) => {
         const survey = { text: req.body.body, title: req.body.title }
         db.collection("surveys").insert(survey, (err, result) => {
