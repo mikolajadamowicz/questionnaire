98bd25df02ac8eb465b019390cffa390996cdcd3
mikolaj adamowicz <mr.ming122@gmail.com>
2018-06-06 19:09:53 +0200

    prawie wszystko juz

--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,18 @@ const appRouter = (app, db) => {
     app.get("/getSurveys", (req, res) => {
       //TODO: zrobic tak zeby zwracal JSONA 
     const collection = db.collection("surveys")
+      const cursor = collection.find({}).sort({ $natural: -1 })
+    cursor.toArray((err, results) => {
+      if (err) throw err
+      console.log("%j", results)
+      res.setHeader("Content-Type", "application/json")
+      res.status(200).send(results)
+    })
+    })
+
+  app.get("/getNewestSurvey", (req, res) => {
+    //TODO: zrobic tak zeby zwracal JSONA 
+    const collection = db.collection("surveys")
     const cursor = collection
       .find()
       .limit(1)
@@ -20,12 +32,12 @@ const appRouter = (app, db) => {
       res.setHeader("Content-Type", "application/json")
       res.status(200).send(results)
     })
-    })
-
+  })
     app.put("/answer/:id", (req, res) => {
       const id = req.params.id
       const details = { _id: new ObjectID(id) }
       const answer = { 
+        // _id: req.body.id,
         text: req.body.text, 
         title: req.body.title,
         forCount: req.body.forCount,
@@ -36,14 +48,18 @@ const appRouter = (app, db) => {
         if (err) {
           res.send({ error: "An error has occurred" })
         } else {
-          // console.log(req.body)
           res.status(200).send(answer)
         }
       })
     })    
 
     app.post("/addSurvey", (req, res) => {
-        const survey = { text: req.body.body, title: req.body.title }
+        const survey = { 
+          text: req.body.body, 
+          title: req.body.title,
+          forCount: req.body.forCount,
+          againstCount: req.body.againstCount
+        }
         db.collection("surveys").insert(survey, (err, result) => {
           if (err) {
             res.send({ error: "An error has occurred" })
